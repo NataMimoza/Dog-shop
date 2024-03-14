70f24f21647ed3d1e529d4e52c4d8c6ca7d6c1f6
NataMimoza <alinatali19@icloud.com>
2024-03-14 16:28:36 +0200

    added project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  // mousewheel: true,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-next',
+  },
+});
+
+
+
